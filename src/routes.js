384bd50842bc3b73a0f82573cca677cacbfbbb65
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2019-08-25 23:01:40 -0300

    feat: linear gradient background for routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 export default createAppContainer(
-  createBottomTabNavigator({
+  createSwitchNavigator({
     SignIn,
     SignUp,
   })
