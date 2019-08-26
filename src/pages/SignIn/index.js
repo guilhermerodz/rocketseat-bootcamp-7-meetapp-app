3cd9a94e6de40a84b08197d9199779d2da8a8664
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2019-08-26 00:15:30 -0300

    feat: Sign In page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Type your name"
-      />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Your e-mail"
+          />
 
-      <Button>Login</Button>
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Your secret password"
+          />
+
+          <SubmitButton onPress={() => {}}>Login</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Create my account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
