f9f79c757a88698894dba7f689b5ba9be056d317
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-08-28 23:05:00 -0300

    feat: added initial route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,28 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignUp,
-    SignIn,
-  })
-);
+import Dashboard from './pages/Dashboard';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignUp,
+          SignIn,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
